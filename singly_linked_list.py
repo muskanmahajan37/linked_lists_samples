b7c14c01c76974726d447125b7ae8499d02c3f75
AG16000 <rohankumar997>
2020-11-11 12:54:28 +0530

    singly linked list

---
+++ singly_linked_list.py
@@ -0,0 +1,34 @@
+class node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+
+
+class Linkedlist:
+    def __init__(self):
+        self.start = None
+    
+    def Display_list(self):
+        pass
+
+    def count_nodes(self):
+        pass
+
+    def search(self,x):
+        pass
+
+    def insert_at_begining(self,data):
+        pass
+
+    def insert_at_end(self,data):
+        pass
+
+    def insert_after(self,data,x):
+        pass
+
+    def insert_before(self,data,x):
+        pass
+
+    def insert_at_position(self,data,k):
+        pass
